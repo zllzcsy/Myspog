cce9ab8b7a3ec502fc41d66f8f62fcb0040bd4f6
zllzcsy <zllzcsy@163.com>
2017-11-10 14:47:38 +0800

    first commist

---
+++ src/main/java/myspog/config/MyspogWebApplicationInitializer.java
@@ -0,0 +1,24 @@
+package myspog.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import myspog.web.WebConfig;
+
+public class MyspogWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] { RootConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
